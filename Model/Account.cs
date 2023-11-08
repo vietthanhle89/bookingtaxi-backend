e434a38d4d742cb0d7039e58c4629eacffc83881
Thanh Le <w2oss01.2023@gmail.com>
2023-11-08 22:36:31 +0700

    init Model and Service

---
+++ Model/Account.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Net;
+using System;
+using System.Numerics;
+using System.Security.Principal;
+using System.Text.Json.Serialization;
+
+namespace bookingtaxi_backend.Models
+{
+    
+
+}
+
+
+
+
+
+
---
+++ Model/Role.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace bookingtaxi_backend.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Role
+    {
+        [BsonId]
+        [BsonElement("_id")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Name { get; set; } = null!;
+        public bool Deleted { get; set; }
+
+        public Role() { Deleted = false; }
+        public Role(string name) { Name = name; Deleted = false; }
+    }
+
+
+
+    public static class ROLEID {
+        public static string DRIVER = "000000000000000000000001";
+        public static string ADMIN = "000000000000000000000002";
+        public static string CUSTOMER = "000000000000000000000003";
+        public static string SUPPORTER = "000000000000000000000004";
+    }
+}
